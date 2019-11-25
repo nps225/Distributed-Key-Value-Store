a7d0e719f43271627a2210213c66edfb2b949680
npsheth <npsheth@ucsc.edu>
2019-11-24 16:11:47 -0800

    added the most updated version

--- replica.sh
+++ replica.sh
@@ -4,12 +4,13 @@
 path_to_dockerfile="."
 
 docker network create --subnet=10.10.0.0/16 kv_subnet
-docker build -t kv-store:3.0 $path_to_dockerfile
+docker build -t kv-store:4.0 $path_to_dockerfile
 
 # example node addresses
 addr1="10.10.0.2:13800"
 addr2="10.10.0.3:13800"
 addr3="10.10.0.4:13800"
+rep= "1"
 
 # convenience variables
 initial_full_view="${addr1},${addr2}"
@@ -19,12 +20,12 @@ nohup docker run --name="node1"        --net=kv_subnet     \
            --ip=10.10.0.2        -p 13802:13800      \
            -e ADDRESS="${addr1}"                     \
            -e VIEW=${initial_full_view}              \
-           -e REPL_FACTOR="1"                        \
-           kv-store:3.0 &
+           -e REPL_FACTOR=${rep}                     \
+           kv-store:4.0 &
 
-docker run --name="node2"        --net=kv_subnet     \
+nohup docker run --name="node2"        --net=kv_subnet     \
            --ip=10.10.0.3        -p 13803:13800      \
            -e ADDRESS="${addr2}"                     \
            -e VIEW=${initial_full_view}              \
-           -e REPL_FACTOR="1"                        \
-           kv-store:3.0 &
+           -e REPL_FACTOR=${rep}                     \
+           kv-store:4.0 &
