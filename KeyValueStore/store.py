cdb7641c190ec555732ead79920c4c89e8ff1939
fasaeed <farhansaeed1999@gmail.com>
2019-11-29 21:18:39 -0800

    added structure of how to do things in gossip, fill in rest

--- KeyValueStore/store.py
+++ KeyValueStore/store.py
@@ -1,100 +1,89 @@
-#key-value store class
+# key-value store class
 class Store:
-   dict
+    dict
 
-   #INIT FUNC
-   def __init__(self):
-      #first we need to initialize our dictionary
-      self.dict = {}
-      self.clock = {}
+    # INIT FUNC
+    def __init__(self):
+        # first we need to initialize our dictionary
+        self.dict = {}
+        self.clock = {}
 
+    # INSERT
+    # returns if it already existed -- server side should handle the errors
+    #        technically server should stop errors before it is even placed
+    #        in key value store
+    # this function should be implemented for updates as well
+    def upsertValue(self, key, value):
+        # check to see if key exists
+        exists = key in self.dict
+        # place the value/new value
+        self.dict[key] = value
+        return exists
 
-   #INSERT
-   #returns if it already existed -- server side should handle the errors
-   #        technically server should stop errors before it is even placed 
-   #        in key value store
-   #this function should be implemented for updates as well
-   def upsertValue(self,key,value):
-      #check to see if key exists
-      exists = key in self.dict
-      #place the value/new value
-      self.dict[key] = value
-      return exists
-   
-   #Grabs the vector clock
-   def upsertVC(self,key,clock):
-      self.clock[key] = clock
-      return clock
-   
-   
-   #GET
-   #returns a tuple (a,b,c)
-   #a = if it exists or not
-   #b = value
-   #c = return code
-   #Error handling
-   #a = false
-   #b = "Key does not exist"
-   #c = 404
-   def getValue(self,key):
-      exists = key in self.dict
-      #if it is present
-      if(exists):
-         code = 200
-         value = self.dict[key]
-      else:
-         #if it isn't present
-         code = 404
-         value = "Key does not exist"
-      
-      return (exists,value,code)
-   
-   #Get the clock value
-   def getClock(self,key):
-      return self.clock[key]
+    # Grabs the vector clock
+    def upsertVC(self, key, clock):
+        self.clock[key] = clock
+        return clock
 
-   #DELETE
-   #returns a boolean -- true if exists -- false if not
-   def deleteValue(self,key):
-      exists = key in self.dict
-      if(exists):
-         self.dict.pop(key)
-         return exists
-      #if dne -- stop here
-      return exists
+    # GET
+    # returns a tuple (a,b,c)
+    # a = if it exists or not
+    #b = value
+    # c = return code
+    # Error handling
+    #a = false
+    #b = "Key does not exist"
+    #c = 404
+    def getValue(self, key):
+        exists = key in self.dict
+        # if it is present
+        if(exists):
+            code = 200
+            value = self.dict[key]
+        else:
+            # if it isn't present
+            code = 404
+            value = "Key does not exist"
 
-   #return our kv-store
-   def returnStore(self):
-      l = []
-      for key in self.dict.keys():
-         l.append((key,self.dict[key]))
-      return l
+        return (exists, value, code)
 
-   def returnClock(self):
-     l = []
-     for key in self.clock.keys():
-        l.append((key,self.clock[key]))
-     return l 
+    # Get the clock value
+    def getClock(self, key):
+        return self.clock[key]
 
+    # DELETE
+    # returns a boolean -- true if exists -- false if not
+    def deleteValue(self, key):
+        exists = key in self.dict
+        if(exists):
+            self.dict.pop(key)
+            return exists
+        # if dne -- stop here
+        return exists
 
-   def returnTables(self):
-      return (self.returnStore(),self.returnClock())
+    # return our kv-store
+    def returnStore(self):
+        l = []
+        for key in self.dict.keys():
+            l.append((key, self.dict[key]))
+        return l
 
+    def returnClock(self):
+        l = []
+        for key in self.clock.keys():
+            l.append((key, self.clock[key]))
+        return l
 
-   def returnTablesDict(self):
-      return (self.dict,self.clock)
+    def returnTables(self):
+        return (self.returnStore(), self.returnClock())
 
-   def comparison(self,store,clock):
-      if(self.dict == store): #later make it so you check if the clock is the same as well
-         return True#means we are up to date
-      else:#not up to date
-         #we will bring our dictionary up to date
-         keys = store.keys()
-         for i in keys:
-            if(self.dict.get(i) != store.get(i)):
-               #update vector clock?
-               self.dict[i] = store[i]
-            elif self.dict.get(i) == store.get(i):
+    def returnTablesDict(self):
+        return (self.dict, self.clock)
+
+    def comparison(self, store, clock):
+        if(self.dict == store):  # later make it so you check if the clock is the same as well
+            keys = store.keys()
+            for i in keys:
                selfClock = self.getClock(i)
                storeClock = store.getClock(i)
                sum1 = sum(selfClock)
@@ -105,4 +94,24 @@ class Store:
                   print('self outdated')
                else:
                   print('uh oh timestamp time')
-         return False
+
+            return True  # means we are up to date
+        else:  # not up to date
+            # we will bring our dictionary up to date
+            keys = store.keys()
+            for i in keys:
+                if(self.dict.get(i) != store.get(i)):
+                    # update with clock
+                    self.dict[i] = store[i]
+                elif self.dict.get(i) == store.get(i):
+                    selfClock = self.getClock(i)
+                    storeClock = store.getClock(i)
+                    sum1 = sum(selfClock)
+                    sum2 = sum(storeClock)
+                    if sum1 > sum2:
+                        print('store outdated')
+                    elif sum1 < sum2:
+                        print('self outdated')
+                    else:
+                        print('uh oh timestamp time')
+            return False
