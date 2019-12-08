99e0f43b3f7e614e96ec2ee9812f1a400449fd4f
root <root@GNEDTSCB1245.localdomain>
2019-12-07 16:37:13 -0800

    done

--- our.py
+++ our.py
@@ -16,8 +16,8 @@ def simplePUT(port,value,data):
    temp = temp = (formatResult(requests.put(url, headers={
                 'Content-Type': 'application/json'}, json=data)))
    a,b = temp
-#    print(a)
-#    print(b)
+   print(a)
+   print(b)
    return a.get("causal-context")
 
 #GET REQUEST
@@ -26,17 +26,20 @@ def simpleGET(port,value,data):
    temp = temp = (formatResult(requests.get(url, headers={
                 'Content-Type': 'application/json'}, json=data)))
    a,b = temp
-#    print(a)
-#    print(b)
+   print(a)
+   print(b)
    return a.get("causal-context")
 
 def simpleViewChange(port,data):
    url = "http://localhost:"+ str(port) +"/kv-store/view-change"
    temp = temp = (formatResult(requests.put(url, headers={
                 'Content-Type': 'application/json'}, json=data)))
+                
    a,b = temp
+   print(a)
+   print(b)
 
-   return a,b
+   return  a.get("causal-context")
 #helper function
 def formatResult(result):
     status_code = result.status_code
@@ -54,19 +57,70 @@ def formatResult(result):
 ##HERE IS WHERE OUR TESTS WILL GO
 data = {"value":"2","causal-context":c1}
 c1 = simplePUT(13802,"a",data)
-# print(c1)
+print(c1)
 data = {"value":"1","causal-context":c1}
-c1 = simplePUT(13805,"c",data)
+c1 = simplePUT(13802,"b",data)
+print(c1)
+data = {"value":"1000000","causal-context":c1}
+c1 = simplePUT(13804,"a",data)
+print(c1)
+data = {"value":"works","causal-context":c1}
+c1 = simplePUT(13802,"c",data)
+print(c1)
+data = {"value":"this should work","causal-context":c1}
+c1 = simplePUT(13803,"d",data)
+
+data = {"value":"this should work","causal-context":c1}
+c1 = simpleGET(13807,"d",data)
+print(c1, "this should fail")
+
 # print(c1)
-# data = {"value":"1000000","causal-context":c1}
-# c1 = simplePUT(13804,"a",data)
-# print(c1)
-# data = {"value":"works","causal-context":c1}
-# c1 = simplePUT(13802,"c",data)
-# print(c1)
-# data = {"value":"this should work","causal-context":c1}
-# c1 = simplePUT(13803,"d",data)
-# print(c1)
-# view = {"view": ["10.10.0.2:13800","10.10.0.3:13800","10.10.0.4:13800","10.10.0.5:13800","10.10.0.6:13800","10.10.0.7:13800"],"repl-factor": 1,"causal-context":c1}
-# c1 = simpleViewChange(13805,view)
-# print(c1)
+view = {"view": ["10.10.0.2:13800","10.10.0.3:13800","10.10.0.4:13800","10.10.0.5:13800","10.10.0.6:13800","10.10.0.7:13800"],"repl-factor": 1,"causal-context":c1}
+c1 = simpleViewChange(13805,view)
+print(c1)
+
+data = {"value":"this should work","causal-context":c1}
+c1 = simpleGET(13807,"d",data)
+print(c1, "this should win")
+
+
+data = {"value":"where is my sportswagon?","causal-context":c1}
+c1 = simpleGET(13807,"d",data)
+print(c1, "this should VIEW CHANGE WORK, but not find value")
+
+print("Done with basic tests")
+
+
+
+
+data = {"value":"the","causal-context":c1}
+c1 = simplePUT(13802,"a",data)
+print(c1)
+data = {"value":"fox","causal-context":c1}
+c1 = simplePUT(13806,"b",data)
+print(c1)
+data = {"value":"was","causal-context":c1}
+c1 = simplePUT(13807,"a",data)
+print(c1)
+data = {"value":"HAMMOND","causal-context":c1}
+c1 = simplePUT(13804,"c",data)
+print(c1)
+data = {"value":"chaps","causal-context":c1}
+c1 = simplePUT(13803,"d",data)
+print(c1)
+view = {"view": ["10.10.0.2:13800","10.10.0.3:13800","10.10.0.4:13800","10.10.0.5:13800",],"repl-factor": 1,"causal-context":c1}
+c1 = simpleViewChange(13805,view)
+print(c1)
+
+data = {"value":"where is my sportswagon?","causal-context":c1}
+c1 = simpleGET(13807,"d",data)
+print(c1, "this should fail")
+
+data = {"value":"ouch?","causal-context":c1}
+c1 = simpleGET(13803,"d",data)
+print(c1, "this should fail")
+
+
+data = {"value":"ouch?","causal-context":c1}
+c1 = simpleGET(13803,"d",data)
+print(c1, "this should win")
